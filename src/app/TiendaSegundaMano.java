66131d3cf363231c9da618f21161c9f894c9a789
leuaxe <leuaxe-488@hotmail.com>
2019-11-11 17:18:37 -0500

    agrego el metodo que aplica los descuentos a la tienda de segunda mano

--- src/app/TiendaSegundaMano.java
+++ src/app/TiendaSegundaMano.java
@@ -28,13 +28,27 @@ public class TiendaSegundaMano{
 
     public void imprimeEstadisticas(){
         System.out.println("--------------------------------------------------------------------------");
-        System.out.println("---------------Lista de productos de la tienda central -------------------");
+        System.out.println("---------------Lista de productos de la tienda segunda mano -------------------");
         double beneficio = 0.0;
         for (int i = 0; i < stock.length-1; i++) {
             stock[i].imprime();
             beneficio += stock[i].getValor(); //suma el total del valor de los productos
         }
-        System.out.println("El beneficio de la tienda del centro es: " + beneficio);
+        System.out.println("El beneficio de la tienda del segunda mano es: " + beneficio);
         System.out.println("--------------------------------------------------");
+    }
+
+    public void aplicarDescuento(){
+        for (int i = 0; i < stock.length-1; i++){
+            if (stock[i].getClase().compareTo("B") == 0) {
+                //aplica un descuento de 25% a los de clase B
+                stock[i].setValor(stock[i].getValor()*0.75);
+            }
+
+            if (stock[i].getClase().compareTo("C") == 0) {
+                //aplica un descuento de 50% a los de clase C
+                stock[i].setValor(stock[i].getValor()*0.50);
+            }
+        }
     }
 }
