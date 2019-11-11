1497d1c0311ff7176f9690113a90b8cbfe981ade
leuaxe <leuaxe-488@hotmail.com>
2019-11-11 17:05:02 -0500

    creo la clase TeindaSegundaMano que tiene los mismos atributos y metodos que la clase TiendaPrimeraMano

---
+++ src/app/TiendaSegundaMano.java
@@ -0,0 +1,40 @@
+package app;
+
+public class TiendaSegundaMano{
+    private String nombre;
+    private Producto[] stock;
+
+    public TiendaSegundaMano(String nombre, Producto[] stock){
+        this.nombre = nombre;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public Producto[] getStock() {
+        return stock;
+    }
+
+    public void setStock(Producto[] stock) {
+        this.stock = stock;
+    }
+
+    public void imprimeEstadisticas(){
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("---------------Lista de productos de la tienda central -------------------");
+        double beneficio = 0.0;
+        for (int i = 0; i < stock.length-1; i++) {
+            stock[i].imprime();
+            beneficio += stock[i].getValor(); //suma el total del valor de los productos
+        }
+        System.out.println("El beneficio de la tienda del centro es: " + beneficio);
+        System.out.println("--------------------------------------------------");
+    }
+}
