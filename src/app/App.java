8979d7eb807e95b84af28bd476cb1d63d9236bf8
leuaxe <leuaxe-488@hotmail.com>
2019-11-12 13:58:40 -0500

    generamos productos, los almancenamos, los separamos

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,18 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
+        Producto[] lp = GeneradorProductos.generarProductos(10);
+        AlmacenGestion ag = new AlmacenGestion(lp);
+        
+        Producto[] s1 = ag.prepararPedidoTC(3);
+        Producto[] s2 = ag.prepararPedidoSM(3);
+
+        TiendaPrimeraMano t1 = new TiendaPrimeraMano("EmpresaSoftPlanet", s1);
+        TiendaSegundaMano t2 = new TiendaSegundaMano("EmpresaSoftExpresss", s2);
+
+        ag.imprimeListaProductos();
+        t1.imprimeEstadisticas();
+        t2.imprimeEstadisticas();
     }
 }
